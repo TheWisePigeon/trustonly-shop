b22630b1a2ea01f7af1102f8ee2268ba899ab69d
TheWisePigeon <zozozozeph@gmail.com>
2022-04-11 08:02:05 +0000

    starting from scratch
    moving from react to vue

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-  return (
-    <footer className="  text-center w-full fixed bottom-0 bg-emerald-400 ">
-      TrustOnly Shop&copy; 
-    </footer>
-  )
-}
