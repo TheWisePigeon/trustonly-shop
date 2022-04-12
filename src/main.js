928185a15594d8800ac1e4118eeb75e969afb106
TheWisePigeon <zozozozeph@gmail.com>
2022-04-12 22:01:54 +0000

    setting up vuex

--- src/main.js
+++ src/main.js
@@ -1,8 +1,42 @@
 import { createApp } from 'vue'
+import { createStore } from 'vuex'
 import App from './App.vue'
 import router from './routes.js'
 
+const store = createStore({
+    state(){
+        return{
+            isLoggedIn: false,
+            user:{},
+            products:[],
+            cart: [],
+            
+        }
+    },
+    mutations:{
+        login(state, user){
+            state.user = user
+        },
+        logout(state){
+            state.user = {}
+        }
+    },
+    getters:{
+        fetchProducts(_){
+            return 
+        }
+    },
+    actions:{
+        login(context, user){
+            context.commit('login', user)
+        },
+        logout(context){
+            context.commit('logout')
+        }
+    },
+})
 
 const app = createApp(App);
 app.use(router);
+app.use(store);
 app.mount('#app');
